77f926df9a9ac3750fe0d759e1bc60712491fced
Arseniy Gaintsev <arseniy.gaintsev@gmail.com>
2021-11-18 21:19:18 +0300

    commit

--- me/settings.py
+++ me/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'me.middleware.CountRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'me.urls'
